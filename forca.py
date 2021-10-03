dcc71356010e8e18597eccab5aa555d7daf2b338
csdamo <csdamo.cx@gmail.com>
2021-10-02 22:53:26 -0300

    Added the first game.

---
+++ forca.py
@@ -0,0 +1,11 @@
+def jogar():
+
+    print("\033[1;33m*********************************")
+    print('***Bem vindo ao jogo da Forca!***')
+    print("*********************************\033[m")
+
+    print('Fim do jogo!')
+
+
+if __name__ == '__main__':
+    jogar()
